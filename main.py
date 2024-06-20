f43223e33293ce1fe361eb609eca8406aae302d0
Haim-Pollak <ch324id@gmail.com>
2024-06-21 02:47:38 +0300

    Replaces the index with a letter if it is a good guess

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 def hidden_word(word: str) -> str:
     """Receives a word and returns it hidden"""
     return len(word) * "*"
+
+
+def check_letter_in_word(word: str, letter: str) -> int:
+    """Gets the word when it is hidden and the guess of the letter
+    and checks if it is a true guess,
+    if so it replaces the index with the letter and returns it
+    if not it returns a level"""
+
+    if letter in word:
+        for char in range(len(word)):
+            if word[char] == letter:
+                guess = word.replace(word[char], letter)
+                return guess
+    return False
